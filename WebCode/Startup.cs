70f4959af329003c161864eb2c2ed3cdc753053c
claudirkraulich <claudirrogerio@gmail.com>
2020-06-17 01:27:46 -0300

    Criado banco SQL Origens e CRUD Origens

--- WebCode/Startup.cs
+++ WebCode/Startup.cs
@@ -37,7 +37,8 @@ namespace WebCode
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebCodeContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebCodeContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebCodeContext"), builder =>
+                        builder.MigrationsAssembly("WebCode")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
