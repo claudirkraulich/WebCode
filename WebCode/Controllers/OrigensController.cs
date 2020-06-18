d5ae6547c1416213680ac9fa7774259947dc976d
claudirkraulich <claudirrogerio@gmail.com>
2020-06-18 01:44:49 -0300

    Criado Demandas Controller

--- WebCode/Controllers/OrigensController.cs
+++ WebCode/Controllers/OrigensController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using WebCode.Data;
 using WebCode.Models;
 
 namespace WebCode.Controllers
